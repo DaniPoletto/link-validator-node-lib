56a8c6ed2a834c48d1fcc72e165e56c37c246fe9
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-09-28 20:25:28 -0300

    incluindo tratamento de erros

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
+}
+
 function pegaArquivo(caminhoDoArquivo) {
     const enconding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, enconding, (_, texto) => {
+    fs.readFile(caminhoDoArquivo, enconding, (erro, texto) => {
+        if (erro) {
+            trataErro(erro);
+        }
         console.log(chalk.green(texto));
     })
 }
