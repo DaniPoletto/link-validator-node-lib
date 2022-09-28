50f25a9242d99dacf4abd5a1bfb80e42dac07d41
danipoletto <danielapoletto_bep@hotmail.com>
2022-09-28 17:43:06 -0300

    mais exemplos de uso da bilioteca chalk

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import chalk from 'chalk';
-
-console.log(chalk.blue('Hello world!'));
+//encadear métodos para colorir texto, cor de fundo e texto em negrito
+console.log(chalk.blue.bgWhite.bold('Alura'));
+//receber múltiplos argumentos
+console.log(chalk.blue('curso', 'de', 'Node.js'));
+//métodos aninhados
+console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
+// uso de template strings e placeholders
+console.log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
