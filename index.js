934f7a436b5829d19360f9616bd6d7825b9997ef
danipoletto <danielapoletto_bep@hotmail.com>
2022-09-28 17:56:50 -0300

    pegando conteudo do arquivo md e exibindo no terminal

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
+import fs from 'fs';
 import chalk from 'chalk';
-//encadear métodos para colorir texto, cor de fundo e texto em negrito
-console.log(chalk.blue.bgWhite.bold('Alura'));
-//receber múltiplos argumentos
-console.log(chalk.blue('curso', 'de', 'Node.js'));
-//métodos aninhados
-console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
-// uso de template strings e placeholders
-console.log(`
-CPU: ${chalk.red('90%')}
-RAM: ${chalk.green('40%')}
-DISK: ${chalk.yellow('70%')}
-`);
+
+function pegaArquivo(caminhoDoArquivo) {
+    const enconding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, enconding, (_, texto) => {
+        console.log(chalk.green(texto));
+    })
+}
+
+pegaArquivo('./arquivos/texto.md');
