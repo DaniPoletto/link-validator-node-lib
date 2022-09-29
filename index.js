f76f3f08a550239da2b9b44366f729ed8e873cde
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-09-28 21:01:25 -0300

    implementando async/await

--- index.js
+++ index.js
@@ -5,21 +5,25 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
 }
 
-function pegaArquivo(caminhoDoArquivo) {
-    const enconding = 'utf-8';
-    fs.promises.readFile(caminhoDoArquivo, enconding)
-        .then((texto) => console.log(chalk.green(texto)))
-        .catch(trataErro);
+
+//async/await
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const enconding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, enconding);
+        console.log(chalk.green(texto));
+    } catch (erro) {
+        trataErro(erro);
+    }
 }
 
+//promises com then
 // function pegaArquivo(caminhoDoArquivo) {
 //     const enconding = 'utf-8';
-//     fs.readFile(caminhoDoArquivo, enconding, (erro, texto) => {
-//         if (erro) {
-//             trataErro(erro);
-//         }
-//         console.log(chalk.green(texto));
-//     })
+//     fs.promises.readFile(caminhoDoArquivo, enconding)
+//         .then((texto) => console.log(chalk.green(texto)))
+//         .catch(trataErro);
 // }
 
 pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
