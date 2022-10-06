f62266dbdeb8e1a513d2e65070a3f50f87e418cb
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-10-06 14:45:15 -0300

    criando função pafra imprimir lista de links

--- src/cli.js
+++ src/cli.js
@@ -4,18 +4,21 @@ import pegaArquivo from "./index.js";
 
 const caminho = process.argv;
 
+function imprimeLista(resultado) {
+    console.log(chalk.yellow('lista de links'), resultado)
+}
+
 async function processaTexto(argumentos) {
     const caminho = argumentos[2];
 
     if (fs.lstatSync(caminho).isFile()) {
         const resultado = await pegaArquivo(caminho);
-        console.log(chalk.yellow('lista de links'), resultado);
+        imprimeLista(resultado);
     } else if (fs.lstatSync(caminho).isDirectory()) {
         const arquivos = await fs.promises.readdir(caminho);
         arquivos.forEach(async (nomeDeArquivo) => {
             const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
-            console.log(`${caminho}/${nomeDeArquivo}`);
-            console.log(lista);
+            imprimeLista(lista);
         })
     }
 
